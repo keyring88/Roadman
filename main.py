9e13949ccb6e33cb1928676e63f11275a454efb2
keyring88 <k3yr1ng@protonmail.com>
2022-05-14 21:25:20 +0100

    committed

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+import discord
+import random
+
+
+
+def randnum(fname):
+	lines=open(fname).read().splitlines()
+	print(lines)
+	return random.choice(lines)
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$roadman'):
+        await message.channel.send(randnum('quotes.txt'))
+
+client.run(os.getenv('TOKEN'))
